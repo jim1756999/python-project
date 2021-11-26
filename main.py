368e4cb841ee9e3d96522d42122157cd6f0d87ca
jim1756999 <490854213@qq.com>
2021-11-26 19:42:46 +0800

    add log system

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ import sys
 import pygame
 import pygame_menu
 from pygame.locals import *
-# doctest
+# debug
 import doctest
+import time
 
 
+# GUI
 class window:
+
     # Parameters
     screen = pygame.display.set_mode((853, 480))
 
@@ -19,43 +22,47 @@ class window:
     pygame.display.set_caption('Snake')
     screen_size = screen.get_size()
 
-class level_:
 
+    # Main menu and submenu at the start of game
+    def main_menu():
+        # Const
+        level_list = [1,2,3,4,5,6,7]
+
+        # Customized theme
+        myfont=pygame.font.get_default_font()
+        mytheme = pygame_menu.Theme(background_color=(0, 0, 0, 0), # transparent background
+                    # title_shadow=True,
+                    title_background_color=(4, 47, 126),
+                    # font=myfont
+                    )
+
+        # Create menu
+        main_menu = pygame_menu.Menu(' Snake', 400, 300,theme=pygame_menu.themes.THEME_BLUE)
+        level_menu = pygame_menu.Menu(' Level', 400, 300,theme=pygame_menu.themes.THEME_BLUE)
+        print("[DEBUG] Menu")
+
+        # Submenu contents
+        for x in level_list:
+            level_menu.add.button(f"Level {x}", choose_level, x)
+        
+        # Main menu contents
+        main_menu.add.text_input('Name : ', default='Player')
+        main_menu.add.button('Play', start_game)
+        main_menu.add.button('Level', level_menu)
+        main_menu.add.button('Quit', sys.exit)
+        main_menu.mainloop(window.screen)
+
+
+class level_:
     print()
 
+
 def main():
+    timestamp.init()
+    print("[MAIN] Program started")
     doctest.testmod()
-    main_menu()
+    window.main_menu()
     exit(0)
-
-# Main menu and submenu at the start of game
-def main_menu():
-    # Const
-    level_list = [1,2,3,4,5,6,7]
-
-    # Create customized theme
-    # myfont=
-    # mytheme = pygame_menu.Theme(background_color=(0, 0, 0, 0), # transparent background
-    #             title_shadow=True,
-    #             title_background_color=(4, 47, 126),
-    #             font=myfont
-    #             )
-
-    # Create menu
-    main_menu = pygame_menu.Menu(' Snake', 400, 300,theme=pygame_menu.themes.THEME_BLUE)
-    level_menu = pygame_menu.Menu(' Level', 400, 300,theme=pygame_menu.themes.THEME_BLUE)
-
-    # Submenu contents
-    for x in level_list:
-        level_menu.add.button(f"Level {x}", choose_level, x)
-    
-    # Main menu contents
-    main_menu.add.text_input('Name : ', default='Player')
-    main_menu.add.button('Play', start_game)
-    main_menu.add.button('Level', level_menu)
-    main_menu.add.button('Quit', sys.exit)
-    main_menu.mainloop(window.screen)
-
 
 
 def start_game():
@@ -63,23 +70,55 @@ def start_game():
     print("[DEBUG] Start game")
     pass
 
+
 def choose_level(level_number):
     # TODO 处理关卡选择
     '''
     >>> choose_level(1)
-    [DEBUG] Choosed level: 1
+    [MENU] Choosed level: 1
     '''
+
     global choosed_level
     choosed_level = level_number
-    print("[DEBUG] Choosed level:", choosed_level)
-    
-# Module for test
+
+    logt = f"[MENU] Choosed level: {choosed_level}"
+    log(logt)
+    print("[MENU] Choosed level:", choosed_level)
+
+
+# Module for test use
 def test(i):
     '''
     >>> test(1)
     [DEBUG] Output is 1
     '''
     print("[DEBUG] Output is",i)
+
+
+# Debug
+# Log
+def log(logt=""):
+    time_now = timestamp.calc()
+    logt = f"[{time_now}] {logt}\n"
+    logf = open("log.txt", mode='a')
+    logf.write(logt)
+
+
+# Timestamp
+class timestamp:
+    def init():
+        global time_start
+        time_start = time.time()
+        log()
+        logt = "[MAIN] Program started"
+        log(logt)
+    def calc():
+        time_now = time.time()
+        start_t = time_now - time_start
+        start_time = round(start_t, 2)
+        return start_time
+    
+
 
 
 if __name__ == '__main__': main()
